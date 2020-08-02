1207a7849874546182ba25a02def6df84796a774
limingyue5 <limingyue5@xiaomi.com>
2020-08-02 18:19:58 +0800

    handler,主线程子线程，bitmap加载
    Signed-off-by: limingyue5 <limingyue5@xiaomi.com>

--- app/src/main/java/com/example/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/androiddemo/MainActivity.java
@@ -2,22 +2,28 @@ package com.example.androiddemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.androiddemo.activity.SecondActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.IOException;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.Toast;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "test";
+    private OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +40,41 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        // 去掉状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // vebView基本使用
+//        WebView webView = findViewById(R.id.webview);
+//        webView.loadUrl("https://www.jianshu.com/p/735a49865211");
 
-        WebView webView = findViewById(R.id.webview);
-        webView.loadUrl("https://www.jianshu.com/p/735a49865211");
+        findViewById(R.id.btn_open_second).setOnClickListener(this);
+        findViewById(R.id.btn_get_networkdata).setOnClickListener(this);
 
-        openSecond();
+    }
 
+    public void getNetwokData() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                Request request = new Request.Builder()
+                        .url("http://www.baidu.com")
+                        .build();
+                Response response = null;
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String result = response.body().toString();
+                Log.d(TAG, result);
+            }
+        }.start();
 
     }
 
     public void openSecond() {
-        Button btn_open_second = findViewById(R.id.btn_open_second);
-        btn_open_second.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -74,5 +97,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_open_second:
+                openSecond();
+                break;
+            case R.id.btn_get_networkdata:
+                getNetwokData();
+                break;
+            default:
+                break;
+        }
     }
 }
